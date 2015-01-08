9440b093bb851f1c8ca5fb69796cf5c6d8077d5e
kmbrickel <kmbrickel@fullsail.edu>
2015-01-07 23:09:59 -0500

    Test the js
    Test the js

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,16 @@
+/*
+Kelsey M. Brickel
+SDI 03
+Expressions Worksheet
+1-7-15
+ */
+
+/*Choose a problem to solve
+Dog Years
+Calculate how old Sparky is in dog years based on his actual age
+Sparky's actual age is 4
+
+test to see if the js is connected
+ */
+
+alert("Hello world!");
