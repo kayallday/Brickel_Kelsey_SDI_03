cf3fbb1cca62b1e34c41add7708e5d790f026214
kmbrickel <kmbrickel@fullsail.edu>
2015-01-11 00:41:52 -0500

    Validate the variable
    Validate the variable

--- Brickel_Kelsey_Expressions_Assignment/js/script.js
+++ Brickel_Kelsey_Expressions_Assignment/js/script.js
@@ -28,6 +28,19 @@ while (userName==="") {
 console.log("The user's name is " + userName + ".");
 
 //Create a variable to ask the user for their confidence about the task
-var conFidence = prompt("Okay " + userName + ", in order to figure out how likely you are to put off this task, we need to know how confident you feel about this task. \n On a scaled of 0-10 (1: not confident at all, and 10: completely confident) enter your confidence level.");
+var conFidence = prompt("Okay " + userName + ", in order to figure out how likely you are to put off this task, we need to know how confident you feel about this task. \n On a scaled of 1-10 (1: not confident at all, and 10: completely confident) enter your confidence level.");
 
-//Validate the variable by ensuring the user enters in a value
+//Validate the variable by ensuring the user enters in a value and that it is a number
+while (conFidence==="" || isNaN(conFidence)) {
+
+    if (conFidence==="") {
+        conFidence = prompt("Whoops! Please enter in a value.");
+    } else {
+        conFidence = prompt("Almost! We need a numerical value, from 1-10.");
+    }
+}
+
+
+
+
+
