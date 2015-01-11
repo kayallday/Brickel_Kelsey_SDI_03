9840c15323fa256bc5a6e65500a3ed19914adcaa
kmbrickel <kmbrickel@fullsail.edu>
2015-01-11 00:18:17 -0500

    Create files for Expressions Assignment
    Create files for Expressions Assignment

---
+++ Brickel_Kelsey_Expressions_Assignment/js/script.js
@@ -0,0 +1,17 @@
+/*
+Kelsey M. Brickel
+SDI 03
+01-11-15
+Professor Dan Williams
+
+Calculate the likelihood of procrastination of a certain task
+
+Develop a formula for the task
+
+ D = CV/IP
+
+ Desirability = Confidence(Value)/Immediacy(Procrastination)
+
+ If the desirability is low then most likely you will procrastinate to complete the task
+
+ */
