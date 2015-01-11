0cb9d292242629fd05a6f0eb1e2ff55629fb89a9
kmbrickel <kmbrickel@fullsail.edu>
2015-01-11 00:26:59 -0500

    Ensure the user enters in a name
    Ensure the user enters in a name

--- Brickel_Kelsey_Expressions_Assignment/js/script.js
+++ Brickel_Kelsey_Expressions_Assignment/js/script.js
@@ -14,4 +14,12 @@ Develop a formula for the task
 
  If the desirability is low then most likely you will procrastinate to complete the task
 
+Create a variable for the users name
  */
+var userName = prompt("Hello, what is your name?");
+
+//Ensure the user enters in a value
+
+while (userName==="") {
+    userName = prompt("Whoops, we want to be formal, and address you by your name. \n Please enter your name.");
+}
