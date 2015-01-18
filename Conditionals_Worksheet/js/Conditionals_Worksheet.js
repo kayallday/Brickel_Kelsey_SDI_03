8758370cd7ff36c9eb07ec0adb08bae7c94ea9b6
kmbrickel <kmbrickel@fullsail.edu>
2015-01-17 20:29:59 -0500

    Create new files and annotate
    Create new files and annotate

---
+++ Conditionals_Worksheet/js/Conditionals_Worksheet.js
@@ -0,0 +1,24 @@
+/*
+Kelsey M. Brickel
+SDI Section #3
+1/17/14
+Conditionals Worksheet
+
+Group 1: Expressions with Conditionals
+Celsius to Fahrenheit converter
+Given:
+- Degrees (in F or C)
+- Unit (a string holding an "F" or a "C")
+
+Result to print:
+"The temperature is X degrees Celsius." Or "The temperature is X degrees Fahrenheit."
+
+Data sets to test:
+
+32F is 0C
+100C is 212F
+90F is 32.22C
+
+
+
+ */
