79e091d55d0f91e895743c2ac343ca5b039030bb
kmbrickel <kmbrickel@fullsail.edu>
2015-01-17 19:55:04 -0500

    Screencast Conditionals
    Screencast Conditionals

--- Conditionals/js/Conditionals.js
+++ Conditionals/js/Conditionals.js
@@ -6,3 +6,15 @@ Conditionals (Screencast)
  */
 //alert("Hello Interweb!!");
 
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+console.log("What comes after");
+
