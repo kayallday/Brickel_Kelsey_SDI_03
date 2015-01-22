2ca34808cbfadb76f484553d30e59536fd54aed8
kmbrickel <kmbrickel@fullsail.edu>
2015-01-22 11:24:18 -0500

    Print the result to the console
    Print the result to the console

--- Conditionals_Worksheet/js/Conditionals_Worksheet.js
+++ Conditionals_Worksheet/js/Conditionals_Worksheet.js
@@ -8,32 +8,33 @@ Group 1: Expressions with Conditionals
 Celsius to Fahrenheit converter
 Given:
 - Degrees (in F or C)
-- Unit (a string holding an "F" or a "C")
-
-Result to print:
-"The temperature is X degrees Celsius." Or "The temperature is X degrees Fahrenheit."
-
-Data sets to test:
-
-32F is 0C
-100C is 212F
-90F is 32.22C
-
-Conversions =
-F = (C * 9/5) + 32
-C = (F - 32) * 5/9
-
-first prompt the user for the degrees
-ask the user if the temperature is celsius or fahrenheit
-use an if statement to determine the calculation
-
-Create a variable for the user to enter in in degrees
+ - Unit (a string holding an "F" or a "C")
+ Result to print:
+ "The temperature is X degrees Celsius." Or "The temperature is X degrees Fahrenheit."
+ Data sets to test:
+ 32F is 0C
+ 100C is 212F
+ 90F is 32.22C
+ Conversions =
+ F = (C * 9/5) + 32
+ C = (F - 32) * 5/9
+ first prompt the user for the degrees
+ ask the user if the temperature is celsius or fahrenheit
+ use an if statement to determine the calculation
+ Create a variable for the user to enter in in degrees
  */
 var degrees = prompt("This is a Fahrenheit and Celsius converter! Please enter in the degrees you want converted.");
 
 //Ensure the user enters in a value and it is a number, using a conditional
 if (degrees==="" || isNaN(degrees)){
+    //Test to see if it is blank or text
     degrees = prompt("Whoops! I think you forgot something!");
-} else {
+} else if(isNaN(degrees)){
     degrees = prompt("Oh no! We need a numerical value!");
+
+
 }
+
+//Print to the console the degrees
+console.log("The degrees to be converted is : " + degrees + ".");
+
--- GoTo Training 3/js/GoToDay3.js
+++ GoTo Training 3/js/GoToDay3.js
@@ -8,3 +8,30 @@ GoTo Training Day #3
 
  //Use an alert to test if the js file is connected to the html
 //alert("Testing to see if the js is connected.");
+
+//Check how many pears are in a fruit bowl
+//Create our "Fruit bowl"
+//Use an array, variable that holds multiple variables
+var bowlOfFruit = ["apple", "pear", "peach"];
+
+console.log(bowlOfFruit);
+
+//Find out how many items are in an array.
+// length property - items in an array
+//Dot Syntax - "use a period"
+console.log(bowlOfFruit.length);
+
+//Call upon a specific item in the array
+console.log(bowlOfFruit[2]);
+
+//Create a variable to hold the total number of pears
+var totalNumPears = 0;
+
+//if(condition to test){Code that will run if condition is true}
+
+// "6" == 6 TRUE - Only looks at value
+// "6" === 6 FALSE Data type is different
+// = is an assignment operator
+if(bowlOfFruit[0] ===){
+
+}
