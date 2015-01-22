a9a8224b163b3688dc4859847937807a57aac2a1
kmbrickel <kmbrickel@fullsail.edu>
2015-01-22 18:29:45 -0500

    Create a variable for fuel efficiency
    Create a variable for fuel efficiency

--- Conditionals_Worksheet/js/Conditionals_Worksheet.js
+++ Conditionals_Worksheet/js/Conditionals_Worksheet.js
@@ -5,5 +5,14 @@ SDI Section #3
 Conditionals Worksheet
 
 Group 1: Expressions with Conditionals
+ Last Chance for Gas!
+ Given:
+ Gas efficiency of the car (mpg)
+ Gauge reading of the gas tank (%)
+ Car's gas tank capacity (gallons)
+ Result to Print:
+ "Yes you can make it without stopping for gas!" or "You only have X gallons of gas in your tank, better get gas now while you can!"
 
-
+*/
+//Create a variable for the fuel efficiency of the car
+var fuelEff = prompt("What is the fuel efficiency of you car? Please enter your answer in mpg.");
