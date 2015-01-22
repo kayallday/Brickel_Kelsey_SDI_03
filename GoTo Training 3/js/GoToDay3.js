b41e957b34828229e351968244bc7a30a4fca284
kmbrickel <kmbrickel@fullsail.edu>
2015-01-21 19:13:59 -0500

    Created Basic files for day 3
    Created Basic files for day 3

---
+++ GoTo Training 3/js/GoToDay3.js
@@ -0,0 +1,10 @@
+
+ /*
+Kelsey M. Brickel
+SDI Section #3
+GoTo Training Day #3
+1/21/15
+ */
+
+ //Use an alert to test if the js file is connected to the html
+//alert("Testing to see if the js is connected.");
