b76f8bdfbea121623ebfcc709142e0deb1a8875c
kmbrickel <kmbrickel@fullsail.edu>
2015-01-22 21:24:35 -0500

    Create a variable for P
    Create a variable for P

--- Brickel_Kelsey_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Brickel_Kelsey_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -4,5 +4,20 @@ SDI Section #3
 1/22/15
 Conditionals Assignment
 
+Happiness Calculator
+
+Formula :
+
+Happiness = P + (5xE) + (3xH)
+
+P - Personal Characteristics
+E - Existence (health, financial stability, friendships)
+H - Higher order
+
+(all based on a scale of 1 - 10)
+
 
  */
+//Begin with creating a variable for P
+var positive = prompt("Using the scale of 1 - 10, with 1 being not at all and 10 being completely, answer the following: \n Do you feel that you have a positive outlook on life, bounce back quickly, and feel in control of your life?");
+
