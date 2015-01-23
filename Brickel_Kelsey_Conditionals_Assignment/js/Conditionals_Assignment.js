5e6fa4de90d690a497703d15c7348a92fee387a0
kmbrickel <kmbrickel@fullsail.edu>
2015-01-22 21:40:05 -0500

    Validate the variable
    Validate the variable

--- Brickel_Kelsey_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Brickel_Kelsey_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -21,3 +21,9 @@ H - Higher order
 //Begin with creating a variable for P
 var positive = prompt("Using the scale of 1 - 10, with 1 being not at all and 10 being completely, answer the following: \n Do you feel that you have a positive outlook on life, bounce back quickly, and feel in control of your life?");
 
+//Validate the variable
+if (positive==="" || isNaN(positive)){
+    positive = prompt("Oh no I think you forgot to enter, in a value.");
+} else {
+    positive = prompt("Rats! We need a numerical value.");
+}
