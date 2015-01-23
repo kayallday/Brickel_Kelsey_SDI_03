947d270dc9a1f9ddbf8cac8182670a46537e34f2
kmbrickel <kmbrickel@fullsail.edu>
2015-01-22 23:28:00 -0500

    Redo the intro and validate existence variable
    Redo the intro and validate existence variable

--- Brickel_Kelsey_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Brickel_Kelsey_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -12,12 +12,16 @@ Happiness = P + (5xE) + (3xH)
 
 P - Personal Characteristics
 E - Existence (health, financial stability, friendships)
-H - Higher order
+H - Higher purpose
 
 (all based on a scale of 1 - 10)
 
 
  */
+//Explain what the calculator is and what it's calculating to the user
+alert("Hello! This is a happiness calculator, it will calculate all the components that make up a person's happiness. When you're ready to begin click okay.");
+
+
 //Begin with creating a variable for P
 var positive = prompt("Using the scale of 1 - 10, with 1 being not at all and 10 being completely, answer the following: \n Do you feel that you have a positive outlook on life, bounce back quickly, and feel in control of your life?");
 
@@ -31,3 +35,17 @@ if (positive==="" || isNaN(positive)){
 
 //Print the result to the console
 console.log("P = " + positive + ".");
+
+//Create a variable for E existence
+var existence = prompt("As with the last question, this is also based on a scale of 1-10, 1 being not at all and 10 being very much so, please answer the following: \n Do you believe your basic needs are being met; health financial stability, personal relationships?");
+
+//Validate the variable, if the prompt is left blank then "Whoops you forgot something"
+if (existence==="" || isNaN(existence)){
+    existence = prompt("Whoops try that one more time!");
+} else {
+    existence = prompt("Oh man, we need a numerical value.");
+}
+
+
+
+
