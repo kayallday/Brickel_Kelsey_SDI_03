abd07fda5d97ffabf87578eb3b3f956ce942686f
kmbrickel <kmbrickel@fullsail.edu>
2015-01-22 21:43:19 -0500

    Validate and print the result
    Validate and print the result

--- Brickel_Kelsey_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Brickel_Kelsey_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -21,9 +21,13 @@ H - Higher order
 //Begin with creating a variable for P
 var positive = prompt("Using the scale of 1 - 10, with 1 being not at all and 10 being completely, answer the following: \n Do you feel that you have a positive outlook on life, bounce back quickly, and feel in control of your life?");
 
-//Validate the variable
+//Validate the variable if the prompt is empty "Oh no I think you forgot to tenter in a value
 if (positive==="" || isNaN(positive)){
-    positive = prompt("Oh no I think you forgot to enter, in a value.");
+    positive = prompt("Oh no I think you forgot to enter in a value.");
+    //if prompt is not a number then "Rats! We need a numerical value."
 } else {
     positive = prompt("Rats! We need a numerical value.");
 }
+
+//Print the result to the console
+console.log("P = " + positive + ".");
