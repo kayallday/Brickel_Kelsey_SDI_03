54f8a40d190beb630e679972cf7a35f23f61b176
kmbrickel <kmbrickel@fullsail.edu>
2015-01-28 20:26:03 -0500

    Example problem
    Example problem

--- Functions_Worksheet/js/Functions_Worksheet.js
+++ Functions_Worksheet/js/Functions_Worksheet.js
@@ -6,17 +6,23 @@ Functions Worksheet
 
 
  */
-/*Problem: Calculate the area of a rectangle given the width and height of the rectangle.
+//Problem: Calculate the area of a rectangle given the width and height of the rectangle.
 
 //Calculate the Area of a Rectangle
-    var width = 5;
+var width = 5;
 var height = 6;
 var area = calculateArea(width, height);
-console.log(“The Area of the Rectangle is “ + area);
+console.log("The area of the Rectangle is " + area);
 
 function calculateArea(w, h )
 {
     return w *h;
 }
-    */
 
+
+/*
+Circumference
+Calculate the circumference of a circle
+Parameters for function:
+
+ */
