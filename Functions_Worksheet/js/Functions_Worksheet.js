6a1f4f3e91c3c01adf17b61bf4e3660d6f554c0a
kmbrickel <kmbrickel@fullsail.edu>
2015-01-28 20:06:37 -0500

    Comment out example
    Comment out example

--- Functions_Worksheet/js/Functions_Worksheet.js
+++ Functions_Worksheet/js/Functions_Worksheet.js
@@ -6,3 +6,17 @@ Functions Worksheet
 
 
  */
+/*Problem: Calculate the area of a rectangle given the width and height of the rectangle.
+
+//Calculate the Area of a Rectangle
+    var width = 5;
+var height = 6;
+var area = calculateArea(width, height);
+console.log(“The Area of the Rectangle is “ + area);
+
+function calculateArea(w, h )
+{
+    return w *h;
+}
+    */
+
