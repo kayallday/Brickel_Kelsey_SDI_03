946eb40373d4f061ae763f2a0494069bc7899cbf
kmbrickel <kmbrickel@fullsail.edu>
2015-01-28 21:27:37 -0500

    Create variable for radius
    Create variable for radius

--- Functions_Worksheet/js/Functions_Worksheet.js
+++ Functions_Worksheet/js/Functions_Worksheet.js
@@ -24,5 +24,27 @@ function calculateArea(w, h )
 Circumference
 Calculate the circumference of a circle
 Parameters for function:
+Radius of the circle
+Return:
+Circumference of the circle
+Result to print to the console:
+The circumference of the circle is X
+
+C = Pi * r^2
+
 
  */
+//Prompt the user for the radius of the circle
+var radius = prompt("To calculate the circumference of the circle, we'll need to know the radius. Please enter it below using only numbers.");
+
+//use a while loop to ensure the user enters in a number
+while(radius==="" || isNaN(radius))
+    if(radius===""){
+        prompt("Whoops! I think you forgot something! Try again.");
+    } else {
+        prompt("Rats! This needs to be a number!!");
+    }
+
+//Print the input to the console
+console.log(radius + " is the radius.");
+
